171e0032deaf30975d3b7bd9061e36bcb36efff7
Kris Young <development@Kriss-MacBook-Pro.local>
2026-09-13 23:05:11 +0100

    Tidy up composer test failures

--- app/Models/City.php
+++ app/Models/City.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Database\Factories\CityFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class City extends Model
 {
-    /** @use HasFactory<\Database\Factories\CityFactory> */
+    /** @use HasFactory<CityFactory> */
     use HasFactory;
 
     protected $fillable = [
