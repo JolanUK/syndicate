71a224a5b75dbea6c445a15861ff439c9102bcd3
Kris Young <development@Kriss-MacBook-Pro.local>
2026-09-13 22:48:14 +0100

    Basic implementation of a City model and Filament resource

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ Route::view('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
+
+    // Syndicate-specific routes
+    Route::view('city', 'city')->name('city');
 });
 
 require __DIR__.'/settings.php';
